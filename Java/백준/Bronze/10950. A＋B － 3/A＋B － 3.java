6a0b71596bc7d4be6a958d0ec86064a568e0a47a
Paul Kim <duddn7414@naver.com>
2026-01-13 20:01:17 +0900

    [Bronze V] Title: A+B - 3, Time: 204 ms, Memory: 18464 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int arr[] = new int[T];
+
+        for(int i=0; i<arr.length; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            arr[i] = A+B;
+        }
+        for(int j:arr){
+            System.out.println(j+" ");
+        }
+    }
+}
